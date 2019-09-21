c2d95e033c1f04d2d87114dc28ae67bd7e188699
Budi Syahiddin <budisyahiddin@protonmail.ch>
2019-09-21 10:51:32 +0800

    actors

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use actix::{Actor, Context, Handler, Message, Addr};
 use actix_web::{
     web, App, Error as ActixErr, HttpRequest, HttpResponse, HttpServer, Responder, ResponseError,
 };
@@ -5,10 +6,59 @@ use futures::future::{ok as fut_ok, Either};
 use lru_time_cache::LruCache;
 use lta::bus::bus_arrival::ArrivalBusService;
 use lta::r#async::{bus::get_arrival, lta_client::LTAClient, prelude::*};
+use parking_lot::RwLock;
 use serde::Serialize;
 use std::fmt::Formatter;
-use parking_lot::RwLock;
 use std::{env::var, time::Duration};
+
+type LruCacheU32 = LruCache<u32, TimingResult>;
+
+enum LruMessages {
+    CheckLru(u32),
+    AddLru(u32, TimingResult)
+}
+
+struct LruActor(LruCacheU32);
+
+impl Message for LruMessages::CheckLru {
+    type Result = Result<Option<Vec<ArrivalBusService>>, std::io::Error>;
+}
+
+impl Message for LruMessages::AddLru {
+    type Result = Result<(), std::io::Error>;
+}
+
+impl Actor for LruActor {
+    type Context = Context<Self>;
+
+    fn started(&mut self, ctx: &mut Self::Context) {
+        println!("LruActor started, spawning LruCache!");
+    }
+
+    fn stopped(&mut self, ctx: &mut Self::Context) {
+        println!("LruActor stopped!");
+    }
+}
+
+impl Handler<LruMessages::CheckLru> for LruActor {
+    type Result = Result<Option<Vec<ArrivalBusService>>, std::io::Error>;
+
+    fn handle(&mut self, msg: LruMessages::CheckLru, ctx: &mut Self::Context) -> Self::Result {
+        println!("LruActor CheckLru !");
+        let data = self.0.peek(&msg.0).map(|u| u.data.clone());
+        Ok(data)
+    }
+}
+
+impl Handler<LruMessages::AddLru> for LruActor {
+    type Result = Result<(), std::io::Error>;
+
+    fn handle(&mut self, msg: LruMessages::AddLru, ctx: &mut Self::Context) -> Self::Result {
+        println!("LruActor AddToLruPing!");
+        self.0.insert(msg.0, msg.1);
+        Ok(())
+    }
+}
 
 #[derive(Clone)]
 struct LruState {
@@ -68,26 +118,31 @@ impl LruState {
 }
 
 fn get_timings(
-    lru_state: web::Data<RwLock<LruState>>,
+    client: web::Data<LTAClient>,
+    lru_actor: web::Data<Addr<LruActor>>
 ) -> impl Future<Item = HttpResponse, Error = JustBusError> {
-    let lru_state_2 = lru_state.clone();
-    let inner = lru_state.read();
-    let in_lru = inner.lru.peek(&83139);
 
-    match in_lru {
+    let msg_res_fut = lru_actor.send(LruMessages::CheckLru(83139));
+
+
+
+    match msg_res {
         Some(data) => {
-            println!("Taking from LRU");
+            //            println!("Taking from LRU");
             Either::A(fut_ok(
                 HttpResponse::Ok().json(TimingResult::new(83139, data.clone().data)),
             ))
         }
         None => {
-            println!("Fresh data from LTA");
+            println!(
+                "Fresh data from LTA on Thread: {:?}",
+                std::thread::current().id()
+            );
             Either::B(
-                get_arrival(&inner.client, 83139, None)
+                get_arrival(&client, 83139, None)
                     .then(move |r| {
                         r.map(|r| {
-                           let data = r.services.clone();
+                            let data = r.services.clone();
                             let mut lru_w = lru_state_2.write();
                             lru_w.lru.insert(83139, TimingResult::new(83139, data));
                             r
@@ -100,20 +155,20 @@ fn get_timings(
     }
 }
 
-type LruCacheU32 = LruCache<u32, TimingResult>;
-
 fn main() {
     println!("Starting server @ 127.0.0.1:8080");
+    let api_key = var("API_KEY").unwrap();
+    let ttl = Duration::from_millis(1000 * 15);
+    let lru_cache = LruCacheU32::with_expiry_duration(ttl);
+    let lru_actor = LruActor(lru_cache).start();
+
     HttpServer::new(move || {
         let api_key = var("API_KEY").unwrap();
-        let ttl = Duration::from_millis(1000 * 15);
-        let lru_state = RwLock::new(LruState::new(
-            LruCacheU32::with_expiry_duration(ttl),
-            LTAClient::with_api_key(api_key),
-        ));
+        let lta_client = LTAClient::with_api_key(api_key);
         App::new()
             .route("/api/v1/timings", web::get().to_async(get_timings))
-            .data(lru_state)
+            .data(lta_client)
+            .data(lru_actor)
     })
     .bind("127.0.0.1:8080")
     .unwrap()
